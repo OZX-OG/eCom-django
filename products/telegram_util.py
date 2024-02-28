6cb5318a0a205b19e9db4e92f3197fc839ce64c5
OZX-OG <fanozxgamer@gmail.com>
2024-02-28 19:27:06 +0100

    add .env

--- products/telegram_util.py
+++ products/telegram_util.py
@@ -1,8 +1,16 @@
 from telegram import Bot
+import os
+from dotenv import load_dotenv
+
+# Load environment variables from .env
+load_dotenv()
+
+TELEGRAM_TOKEN = os.getenv('SECRET_KEY')
+TELEGRAM_CHAT_ID = os.getenv('TELEGRAM_CHAT_ID')
 
 async def send_telegram_message(message):
-    bot_token = '5045449249:AAFB1tNU_X8xOQI-rGCWyLhi2JU-RwRrcfU'
-    chat_id = '1234416899'
+    bot_token = TELEGRAM_TOKEN
+    chat_id = TELEGRAM_CHAT_ID
     
     bot = Bot(token=bot_token)
     await bot.send_message(chat_id=chat_id, text=message)
